698aebb2c58d894c77a2e564a72016bae335cb60
tbreuss <thomasbreuss@gmx.ch>
2022-08-21 09:48:42 +0200

    fix: Invalid RSS feed issues

--- views/feed/rss.php
+++ views/feed/rss.php
@@ -5,13 +5,15 @@
  */
 
 use app\models\Search;
+use yii\helpers\Markdown;
 use yii\helpers\Url;
 
 // Siehe http://feeds.bbci.co.uk/news/world/rss.xml
 ?>
 <?= '<?xml version="1.0" encoding="utf-8"?>' ?>
-<rss version="2.0">
+<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
     <channel>
+        <atom:link href="<?= Url::to(['/feed/rss'], true) ?>" rel="self" type="application/rss+xml" />
         <title>Electricbass.ch | Wissen und Lernen</title>
         <link>https://www.electricbass.ch</link>
         <description>Umfangreiche Informationen zum Thema E-Bass mit Artikeln, Lektionen, Bassriffs, einem Glossar, Lernvideos und vielem mehr.</description>
@@ -19,14 +21,14 @@ use yii\helpers\Url;
         <pubDate><?= date(DATE_RSS, time()) ?></pubDate>
         <?php foreach ($query->each() as $row): ?>
             <?php $model = new Search($row); ?>
-        <item>
-            <title><![CDATA[<?= htmlspecialchars($model->title, ENT_XML1) ?>]]></title>
-            <description><![CDATA[<?= htmlspecialchars($model->abstract, ENT_XML1) ?>]]></description>
-            <link><?= Url::to($model->url, true) ?></link>
-            <guid isPermaLink="true"><?= Url::to($model->url, true) ?></guid>
-            <?php $timestamp = strtotime($model->modified) ?>
-            <pubDate><?= ($timestamp === false) ? '' : date(DATE_RSS, $timestamp) ?></pubDate>
-        </item>
+            <item>
+                <title><![CDATA[<?= strip_tags($model->title) ?>]]></title>
+                <description><![CDATA[<?= Markdown::process($model->abstract, 'gfm') ?>]]></description>
+                <link><?= Url::to($model->url, true) ?></link>
+                <guid isPermaLink="true"><?= Url::to($model->url, true) ?></guid>
+                <?php $timestamp = strtotime($model->modified) ?>
+                <pubDate><?= ($timestamp === false) ? '' : date(DATE_RSS, $timestamp) ?></pubDate>
+            </item>
         <?php endforeach; ?>
     </channel>
 </rss>
