1dcceb967e2eaea93ac943c0f66d0c849ae2f13a
tbreuss <thomasbreuss@gmx.ch>
2022-11-30 16:34:50 +0100

    content: Amazon-Ad hinzugefügt

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -16,9 +16,10 @@ use yii\helpers\ArrayHelper;
         </div>
         <div class="col-md-4 sidebar">
             <div class="sidebar__inner">
-            <?php if (!empty($this->blocks['sidebar'])): ?>
-                <?= ArrayHelper::getValue($this->blocks, 'sidebar') ?>
-            <?php endif; ?>
+                <iframe src="https://rcm-eu.amazon-adsystem.com/e/cm?o=3&p=12&l=ur1&category=musicunlimited&banner=1RDMQ3JSAHB2MQ95F7G2&f=ifr&linkID=abf0235934dae54ef61d39d960ed7184&t=electricbas03-21&tracking_id=electricbas03-21" width="300" height="250" scrolling="no" border="0" marginwidth="0" style="border:none;" frameborder="0" sandbox="allow-scripts allow-same-origin allow-popups allow-top-navigation-by-user-activation"></iframe>
+                <?php if (!empty($this->blocks['sidebar'])): ?>
+                    <?= ArrayHelper::getValue($this->blocks, 'sidebar') ?>
+                <?php endif; ?>
             </div>
         </div>
     </div>
