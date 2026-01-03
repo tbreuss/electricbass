46f3d7d7cc3cb0083b017fcf659efe1fd1188fdc
Thomas Breuss <thomasbreuss@gmx.ch>
2026-01-03 13:09:28 +0100

    fix: full width for comments form

--- widgets/views/comments/form.php
+++ widgets/views/comments/form.php
@@ -10,13 +10,13 @@ use app\helpers\Html;
 <div class="comments__form">
 
     <?php if (Yii::$app->session->hasFlash('comment/error')): ?>
-        <div class="flash flash--danger col-12 col-sm-8">
+        <div class="flash flash--danger col-12">
             <?= Yii::$app->session->getFlash('comment/error') ?>
         </div>
     <?php endif; ?>
 
     <?php if (Yii::$app->session->hasFlash('comment/success')): ?>
-        <div class="flash flash--success col-12 col-sm-8">
+        <div class="flash flash--success col-12">
             <?= Yii::$app->session->getFlash('comment/success') ?>
         </div>
     <?php endif; ?>
@@ -30,7 +30,7 @@ use app\helpers\Html;
     <?= Html::beginForm('#form', 'post', ['id' => 'form', 'class' => 'form', 'novalidate' => true]) ?>
 
     <div class="row">
-        <div class="col-12 col-sm-8 form__row">
+        <div class="col-12 form__row">
             <?php $errorClass = $model->hasErrors('text') ? 'is-invalid' : ''; ?>
             <?= Html::label($model->getAttributeLabel('text'), 'text', ['class' => 'form__label']); ?>
             <?= Html::textarea('Comment[text]', $model->text, ['rows' => 6, 'class' => 'form__textarea form-control ' . $errorClass]); ?>
@@ -38,14 +38,14 @@ use app\helpers\Html;
             <div class="form__hint">max. 1000 Zeichen</div>
         </div>
 
-        <div class="col-12 col-sm-8 form__row">
+        <div class="col-12 form__row">
             <?php $errorClass = $model->hasErrors('name') ? 'is-invalid' : ''; ?>
             <?= Html::label($model->getAttributeLabel('name'), 'name', ['class' => 'form__label']); ?>
             <?= Html::textInput('Comment[name]', $model->name, ['class' => 'form__textInput form-control ' . $errorClass]); ?>
             <?= Html::error($model, 'name', ['class' => 'invalid-feedback']); ?>
         </div>
 
-        <div class="col-12 col-sm-8 form__row">
+        <div class="col-12 form__row">
             <?php $errorClass = $model->hasErrors('check') ? 'is-invalid' : ''; ?>
             <div class="form__checkboxWrapper">
                 <?= Html::checkbox('Comment[check]', $model->check, ['id' => 'check', 'class' => 'form__checkbox ' . $errorClass]); ?>
@@ -61,7 +61,7 @@ use app\helpers\Html;
             <?= Html::error($model, 'verifyCode', ['class' => 'invalid-feedback']); ?>
         </div>*/ ?>
 
-        <div class="col-12 col-sm-8 form__row collapsible comments__rules" id="comment-rules">
+        <div class="col-12 form__row collapsible comments__rules" id="comment-rules">
             <p><b>Kommentarregeln</b></p>
             <p><i>Ehrverletzung/Beleidigung</i>: Um einen angenehmen, sachlichen und fairen Umgang miteinander zu gewährleisten, publizieren wir keine Beiträge, die sich im Ton vergreifen. Dazu gehören die Verwendung von polemischen und beleidigenden Ausdrücken ebenso wie persönliche Angriffe auf andere Diskussionsteilnehmer.</p>
             <p><i>Rassismus/Diskriminierung</i>: Es ist nicht erlaubt, Inhalte zu verbreiten, die unter die Schweizerische Rassismusstrafnorm fallen und Personen aufgrund ihrer Rasse, Ethnie oder Religion herabsetzen oder zu Hass aufrufen. Diskriminierende Äusserungen werden nicht publiziert.</p>
