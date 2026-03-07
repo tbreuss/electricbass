f3115afe7d9d5b61574502bbf6edc7bcd870c5be
Thomas Breuss <thomasbreuss@gmx.ch>
2026-03-07 09:22:29 +0100

    fix: remove redirections based on numeric paths

--- features/error/Controller.php
+++ features/error/Controller.php
@@ -4,7 +4,6 @@ namespace app\features\error;
 
 use app\components\Redirect;
 use app\features\error\models\Log4xx;
-use app\features\search\models\Search;
 use Yii;
 use yii\base\InvalidConfigException;
 use yii\web\HttpException;
@@ -19,45 +18,13 @@ final class Controller extends \yii\web\Controller
         } catch (InvalidConfigException $e) {
             $requestUrl = '__InvalidConfigException__';
         }
-        try {
-            $pathInfo = Yii::$app->request->getPathInfo();
-        } catch (InvalidConfigException $e) {
-            $pathInfo = '__InvalidConfigException__';
-        }
+
         $referrer = Yii::$app->request->getReferrer();
 
         $exception = Yii::$app->errorHandler->exception;
 
         if ($exception instanceof HttpException && $exception->statusCode >= 400 && $exception->statusCode < 500) {
-            // - www.electricbass.ch/12994
-            // - www.electricbass.ch/links/le-fay-1525
 
-            if (is_numeric($pathInfo)) {
-                // 2327
-                #if ($pathInfo <= 2327) {
-                #    $pathInfo += 10000;
-                #}
-                $model = Search::find()->where(['id' => $pathInfo])->one();
-                if ($model) {
-                    return $this->redirect($model->url, 301);
-                }
-            }
-
-            // basierend auf context und id
-            /*
-            $id = Yii::$app->db->createCommand('SELECT id FROM oldurl WHERE url=:url')
-                ->bindValue(':url', $pathInfo)
-                ->queryScalar();
-
-            if ($id > 0) {
-                $model = Search::find()->where(['id' => $id])->one();
-                if ($model) {
-                    return $this->redirect($model->url, 301);
-                }
-            }
-            */
-
-            // basierend auf url
             $trimmedRequestUrl = '/' . trim(strval(preg_replace('#/{2,}#', '/', $requestUrl)), '/');
             $redirect = Redirect::findOneByRequestUrl($trimmedRequestUrl);
             if ($redirect) {
