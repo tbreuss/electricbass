e248ff4218011d5436b0dd307a21b85cd62d813e
Thomas Breuss <thomasbreuss@gmx.ch>
2026-03-19 07:19:36 +0100

    wip: quiz

--- features/quiz/views/view.php
+++ features/quiz/views/view.php
@@ -5,8 +5,10 @@
  * @var yii\web\View $this
  */
 ?>
+<div>
 <?php if ($model->widget === 'ReadNote'): ?>
     <?= app\features\quiz\ReadNoteWidget::widget(array_merge($model->widgetOptions, ['nextUrl' => $model->next->url ?? null])); ?>
 <?php elseif ($model->widget === 'WriteNote'): ?>
     <?= app\features\quiz\WriteNoteWidget::widget($model->widgetOptions); ?>
 <?php endif ?>
+</div>
